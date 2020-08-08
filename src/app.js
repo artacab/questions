564bc2dcfbe305028034c5cf21f031b317a6518c
artacab <artacab@gmail.com>
2020-08-08 11:36:43 +0300

    upload

---
+++ src/app.js
@@ -0,0 +1,75 @@
+import {Question} from "./question";
+import './style.css'
+import {createModal, isValid} from "./utils";
+import {authWithEmailAndPassword, getAuthForm} from "./auth";
+
+const form = document.getElementById('form')
+const input = form.querySelector('#quest-input')
+const submitBtn = form.querySelector('#submit')
+const modalBtn = document.getElementById('modal-btn')
+
+window.addEventListener('load', Question.renderList)
+modalBtn.addEventListener('click', openModal)
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+    event.preventDefault()
+    if (isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        }
+        submitBtn.disabled = true
+        Question.create(question).then(() => {
+            input.value = ''
+            input.className = ''
+            submitBtn.disabled = false
+        })
+    }
+}
+
+function openModal() {
+    createModal('Авторизация', getAuthForm())
+    document
+        .getElementById('auth-form')
+        .addEventListener('submit', authFormHandler, {once: true})
+}
+
+function authFormHandler(event) {
+    event.preventDefault()
+    const btn = event.target.querySelector('button')
+    const email = event.target.querySelector('#email').value
+    const password = event.target.querySelector('#password').value
+
+    btn.disabled = true
+    authWithEmailAndPassword(email, password)
+        .then(Question.fetch)
+        .then(renderModalAfterAuth)
+        .then(() => btn.disabled = false)
+}
+
+function renderModalAfterAuth(content) {
+    if(typeof content === 'string') {
+        createModal('Ошибка!', content)
+    } else {
+        createModal('Cписок вопросов', Question.listToHTML(content))
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/question.js
@@ -0,0 +1,68 @@
+export class Question {
+    static create(question) {
+        return fetch('https://js-practice-7d52c.firebaseio.com/questions.json', {
+            method: 'POST',
+            body: JSON.stringify(question),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                question.id = response.name
+                return question
+            })
+            .then(addToLocalStorage)
+            .then(Question.renderList)
+    }
+
+    static fetch(token) {
+        if (!token) {
+            return Promise.resolve('<p class="error">no token</p>')
+        }
+        return fetch(`https://js-practice-7d52c.firebaseio.com/questions.json?auth=${token}`)
+            .then(response => response.json())
+            .then(response => {
+                if (response.error) {
+                    return `<p class="error">${questions.error}</p>`
+                }
+
+                return response ? Object.keys(response).map(key => ({
+                    ...response[key],
+                    id: key
+                })) : []
+            })
+    }
+
+    static renderList() {
+        const questions = getQuestionFromLocalStorage()
+        const html = questions.length ? questions.map(toCard).join('')
+            : `<div class="mui--text-headline">Вопросов нет!</div>`
+        const list = document.getElementById('list')
+        list.innerHTML = html
+    }
+    static listToHTML(questions) {
+        return questions.length
+        ? `<ol>${questions.map(q => `<li>${q.text}</li>`).join('')}</ol>`
+        : `<p>Вопросов пока нету</p>`
+    }
+}
+
+function addToLocalStorage(question) {
+    const all = getQuestionFromLocalStorage()
+    all.push(question)
+    localStorage.setItem('questions', JSON.stringify(all))
+
+}
+
+function getQuestionFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('questions') || '[]')
+}
+
+function toCard(question) {
+    return `<div class="mui--text-black-54">
+                ${new Date(question.date).toLocaleDateString()}
+                ${new Date(question.date).toLocaleTimeString()}
+           </div>
+            <div>${question.text}</div>`
+}
